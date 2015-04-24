dafa8df47e0321a5cf3802ee4f9dff52c2eeadee
Jeffrey Brown <jeffywb@hotmail.com>
2015-04-24 16:00:28 +1200

    Calculate PI

--- app/controllers/pagesController.js
+++ app/controllers/pagesController.js
@@ -2,9 +2,20 @@ var locomotive = require('locomotive')
   , Controller = locomotive.Controller;
 
 var pagesController = new Controller();
-
+var insidecount = 0;
+var outsidecount = 0;
 pagesController.main = function() {
   this.title = 'Enraged Vocabulary Girl';
+  var result = Math.sqrt(Math.pow(Math.random(), 2) + Math.pow(Math.random(), 2))
+  if(result <= 1) {
+	insidecount++;  
+  }
+  else {
+	  outsidecount++;
+  }
+  this.Pi = insidecount / Math.max(outsidecount, 1);
+  this.inside = insidecount;
+  this.outside = outsidecount;
   this.render();
 }
 
